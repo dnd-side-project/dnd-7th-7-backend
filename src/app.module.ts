35cf4e2013988eee1f792ec1fc90595fac2a22b9
YELIN JEONG <jyl7464@gmail.com>
2022-07-17 13:04:17 +0900

    build(docker): add docker environment setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import {User} from "./user/entities/user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQLDB_HOST,
+      port: parseInt(process.env.MYSQLDB_DOCKER_PORT, 10),
+      username: process.env.MYSQLDB_USER,
+      password: process.env.MYSQLDB_PASSWORD,
+      database: process.env.MYSQLDB_DATABASE,
+      entities: [User],
+      synchronize: true, // Fix me : set this value to false when deploy
+    }
+    ),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
