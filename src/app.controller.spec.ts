886f2c8a314007937a8ac8297a0e84182dbef67d
YELIN JEONG <jyl7464@gmail.com>
2022-07-28 23:09:03 +0900

    ci(codecov): change coverage-report-action to codecov-action

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "build-and-deploy test success"', () => {
+      expect(appController.getHello()).toBe('build-and-deploy test success');
     });
   });
 });
