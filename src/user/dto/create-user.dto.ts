84915a2ba39f35f8715325b9f093a07dc140fbf7
YELIN JEONG <jyl7464@gmail.com>
2022-08-06 00:43:24 +0900

    feat(user): add create,update,remove API

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsDate, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  readonly nickname: string;
+
+  @IsString()
+  readonly userId: string;
+
+  @IsString()
+  password: string;
+
+  @IsDate()
+  readonly birthDate: Date;
+
+  @IsString()
+  readonly gender: string;
+
+  @IsString()
+  readonly address: string;
+}
