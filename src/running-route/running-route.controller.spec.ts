eba79577f7d8e50b19ada7618adb9b4e2aa6a167
ParkChaerim <qkrdbsk28@naver.com>
2022-08-06 22:30:47 +0900

    feat(route): test file dependency setting

--- src/running-route/running-route.controller.spec.ts
+++ src/running-route/running-route.controller.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RunningRouteController } from './running-route.controller';
-
+import { NestjsFormDataModule } from 'nestjs-form-data';
+import { RunningRouteService } from './running-route.service';
+import { RunningRoute } from './entities/running-route.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Image } from './entities/image.entities';
+import { RouteTag } from './entities/route-tag.entities';
 describe('RunningRouteController', () => {
   let controller: RunningRouteController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [NestjsFormDataModule],
       controllers: [RunningRouteController],
+      providers: [
+        RunningRouteService,
+        {
+          provide: getRepositoryToken(RunningRoute),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(RouteTag),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Image),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<RunningRouteController>(RunningRouteController);
--- src/running-route/running-route.service.spec.ts
+++ src/running-route/running-route.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RunningRouteService } from './running-route.service';
+import { RouteTag } from './entities/route-tag.entities';
+import { Image } from './entities/image.entities';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { RunningRoute } from './entities/running-route.entity';
 
 describe('RunningRouteService', () => {
   let service: RunningRouteService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RunningRouteService],
+      providers: [
+        RunningRouteService,
+        {
+          provide: getRepositoryToken(RunningRoute),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(RouteTag),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Image),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<RunningRouteService>(RunningRouteService);
