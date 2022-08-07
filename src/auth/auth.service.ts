7320c4304ee6f6936781b911171e848646d51a10
YELIN JEONG <jyl7464@gmail.com>
2022-08-07 20:02:25 +0900

    feat(auth): create login api using JWT

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { ForbiddenException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LoginUserDto } from '../user/dto/login-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(loginUserDto: LoginUserDto): Promise<any> {
+    const user = await this.userRepository.findOneBy({
+      userId: loginUserDto.userId,
+    });
+
+    if (!user) {
+      throw new ForbiddenException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: ['Unregistered user'],
+        error: 'Forbidden',
+      });
+    }
+
+    const isMatch = await bcrypt.compare(loginUserDto.password, user.password);
+
+    if (isMatch) {
+      const { password, ...result } = user;
+      return result;
+    } else {
+      throw new ForbiddenException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: ['Wrong password'],
+        error: 'Forbidden',
+      });
+    }
+  }
+
+  async login(user: any) {
+    const payload = { username: user.name, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { LoginUserDto } from '../../user/dto/login-user.dto';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'userId',
+    });
+  }
+
+  async validate(userId: string, password: string): Promise<any> {
+    const loginUserDto: LoginUserDto = {
+      userId: userId,
+      password: password,
+    };
+
+    const user = await this.authService.validateUser(loginUserDto);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
