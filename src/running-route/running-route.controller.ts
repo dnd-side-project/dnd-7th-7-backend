dd652fa3ed62d97892e438b4d6f58562a9420148
박채림 <chaerim@bagchaelim-ui-MacBookPro.local>
2022-08-10 00:45:24 +0900

    feat(route): add getById API

--- src/running-route/running-route.controller.ts
+++ src/running-route/running-route.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { FormDataRequest } from 'nestjs-form-data';
 import { CreateRunningRouteDto } from './dto/create-running-route.dto';
 import { RunningRouteService } from './running-route.service';
@@ -11,5 +11,10 @@ export class RunningRouteController {
   @FormDataRequest()
   async create(@Body() createRunningRouteDto: CreateRunningRouteDto) {
     return await this.runningRouteService.create(createRunningRouteDto);
+  }
+
+  @Get('/:id')
+  async getById(@Param('id') id: number) {
+    return await this.runningRouteService.getById(id);
   }
 }
